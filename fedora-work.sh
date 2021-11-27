635e9e42f8e08bb61cfe0c66abc0742e7836242a
k-const <78409869+k-const@users.noreply.github.com>
2021-11-27 20:56:59 +0300

    Update fedora-work.sh

--- fedora-work.sh
+++ fedora-work.sh
@@ -6,14 +6,16 @@ dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-
 https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
-dnf update
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
 dnf -y upgrade
 dnf -y groupupdate core
 
 ## installing gnome
-dnf -y install adwaita-qt5 adwaita-qt6 cabextract cups-pdf dnfdragora file-roller-nautilus firewall-config \
-flatpak gdm gnome-shell gnome-terminal gnome-terminal-nautilus gutenprint-cups microsoft-edge-stable \
-nautilus nautilus-extensions nautilus-search-tool thunderbird torbrowser-launcher xorg-x11-font-utils
+dnf -y install adwaita-qt5 adwaita-qt6 cabextract code cups-pdf dnfdragora file-roller-nautilus \
+firewall-config flatpak gdm gnome-shell gnome-terminal gnome-terminal-nautilus gutenprint-cups \
+microsoft-edge-stable nautilus nautilus-extensions nautilus-search-tool thunderbird torbrowser-launcher \
+xorg-x11-font-utils
 rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 dnf -y groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 dnf -y groupupdate sound-and-video
